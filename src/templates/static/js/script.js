02b33774acd7587611e9369986963d91ab6c1811
Robin Read <robin.g.read@gmail.com>
2023-04-16 19:29:06 +0100

    Removed all extra UI junk that we don't need.

--- src/templates/static/js/script.js
+++ src/templates/static/js/script.js
@@ -1,48 +1,6 @@
 $( document ).ready(function() {
-
-    function createCanvas(parent, width, height) {
-     var canvas = document.getElementById("inputCanvas");
-     canvas.context = canvas.getContext('2d');
-     return canvas;
-   }
  
-   function init(container, width, height, fillColor) {
-     var canvas = createCanvas(container, width, height);
-     var ctx = canvas.context;
-     ctx.fillCircle = function(x, y, radius, fillColor) {
-       this.fillStyle = fillColor;
-       this.beginPath();
-       this.moveTo(x, y);
-       this.arc(x, y, radius, 0, Math.PI * 2, false);
-       this.fill();
-     };
-     ctx.clearTo = function(fillColor) {
-       ctx.fillStyle = fillColor;
-       ctx.fillRect(0, 0, width, height);
-     };
-     ctx.clearTo("#fff");
- 
-     canvas.onmousemove = function(e) {
-       if (!canvas.isDrawing) {
-         return;
-       }
-       var x = e.pageX - this.offsetLeft;
-       var y = e.pageY - this.offsetTop;
-       var radius = 10;
-       var fillColor = 'rgb(102,153,255)';
-       ctx.fillCircle(x, y, radius, fillColor);
-     };
-     canvas.onmousedown = function(e) {
-       canvas.isDrawing = true;
-     };
-     canvas.onmouseup = function(e) {
-       canvas.isDrawing = false;
-     };
-   }
- 
-   var container = document.getElementById('canvas');
    let characterBuffer = "";
-   init(container, 200, 200, '#ddd');
 
    function showCharacters(buffer="none") {
     var input = document.getElementById("characters")
@@ -51,17 +9,6 @@ $( document ).ready(function() {
 
    function completeCharacters(buffer) {  
       var payload = buffer;
-    //   var words = ["apple", "banana"];
-      
-    //   $.post('/complete', payload, function(data) {
-    //     // var response = JSON.parse(data);
-    //     words = data.words;
-    //     // Do something with the returned data
-    //     console.log(data);
-    //   }).fail(function(error) {
-    //     // Handle any errors that occur during the request
-    //     console.error(error);
-    //   });
 
       return new Promise(function(resolve, reject) {
         $.post("/complete", payload, function(data) {
@@ -76,19 +23,12 @@ $( document ).ready(function() {
    }
 
    function populateList(words) {
-    // Get a reference to the <ul> element
     var list = document.getElementById('word-list');
     list.innerHTML = '';
 
-    // var words = ['apple', 'banana', 'cherry', 'date', 'elderberry'];
-
-    // Loop through the array of words
     for (var i = 0; i < words.length; i++) {
-      // Create a new <li> element for each word
       var listItem = document.createElement('li');
       listItem.textContent = words[i];
-
-      // Add the <li> element to the <ul> element
       list.appendChild(listItem);
     }
   }
@@ -96,10 +36,6 @@ $( document ).ready(function() {
    $( "#clearButton" ).click(function(){
      characterBuffer = "";
      showCharacters(characterBuffer);
-   });
- 
-   $( "#sendButton" ).click(function(){
-     showCharacters();
    });
 
    $(document).keyup(function(e) {
