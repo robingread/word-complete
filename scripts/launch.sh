c0bb250218ba39ad52bbc460edd64b960493d986
Robin Read <robin.g.read@gmail.com>
2023-04-16 22:49:50 +0100

    Updated the scripts/launch.sh file to automatically pull the latest image, stop any running containers, run a new container and open a web browser.

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -1,4 +1,16 @@
 #! /bin/bash
 
-URL=http:localhost:5000
+set -eu
+
+NAME=word-complete
+IMG=robingread/word-complete:latest
+PORT=5000
+
+docker image pull $IMG
+docker container stop $NAME --time 0 || true
+docker run --name $NAME --detach --rm -p $PORT:$PORT $IMG || true
+
+sleep 2
+
+URL=http://localhost:$PORT
 xdg-open $URL
