b87eab9347e13301ad27265669b82db2199b2df5
Robin Read <robin.g.read@gmail.com>
2023-04-16 17:05:39 +0100

    Working example where I can catch keyevents and update a disabled input box with a character buffer. I can also use the "clear" button to clear the buffer.

--- src/templates/static/js/script.js
+++ src/templates/static/js/script.js
@@ -41,35 +41,26 @@ $( document ).ready(function() {
    }
  
    var container = document.getElementById('canvas');
+   let characterBuffer = "";
    init(container, 200, 200, '#ddd');
- 
-   function clearCanvas() {
-     var canvas = document.getElementById("inputCanvas");
-     var ctx = canvas.getContext("2d");
-         ctx.clearRect(0, 0, canvas.width, canvas.height);
-   }
- 
-   function getData() {
-     var canvas = document.getElementById("inputCanvas");
-     var imageData = canvas.context.getImageData(0, 0, canvas.width, canvas.height);
-     var data = imageData.data;
-     var outputData = []
-     for(var i = 0; i < data.length; i += 4) {
-       var brightness = 0.34 * data[i] + 0.5 * data[i + 1] + 0.16 * data[i + 2];
-       outputData.push(brightness);
-     }
-     $.post( "/postmethod", {
-       canvas_data: JSON.stringify(outputData)
-     }, function(err, req, resp){
-       window.location.href = "/results/"+resp["responseJSON"]["uuid"];  
-     });
+
+   function showCharacters(buffer="none") {
+    var input = document.getElementById("characters")
+    input.value = buffer;
    }
  
    $( "#clearButton" ).click(function(){
-     clearCanvas();
+     characterBuffer = "";
+     showCharacters(characterBuffer);
    });
  
    $( "#sendButton" ).click(function(){
-     getData();
+     showCharacters();
    });
+
+   $(document).keypress(function(e) {
+    var keynum = e.keyCode;
+    characterBuffer += String.fromCharCode(keynum)
+    showCharacters(characterBuffer);
+  });
  });
