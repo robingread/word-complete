8a06baab412d4bf0aec22803d07d29b545809716
Robin Read <robin.g.read@gmail.com>
2023-05-08 16:32:05 +0100

    Updated the scripts/build-deploy.sh script to add both the custom tag and "latest" as the tags attached to the build images.

--- scripts/build-deploy.sh
+++ scripts/build-deploy.sh
@@ -4,7 +4,7 @@ set -eu
 
 BUILDER_NAME=multi-stach
 IMG=robingread/word-complete
-TAG=${1:-latest} 
+TAG=${1} 
 
 echo "Deploying docker image with tag:" ${IMG}:${TAG}
 
@@ -12,4 +12,4 @@ export DOCKER_CLI_EXPERIMENTAL=enabled
 
 docker buildx create --name $BUILDER_NAME --use || true
 docker buildx inspect --bootstrap
-docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t $IMG:$TAG --push .
+docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t $IMG:$TAG -t $IMG:latest --push .
